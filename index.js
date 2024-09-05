1276aa86390e8a0265305cf94b65f7bbdc43c74b
GustavoMachadolindaoo <127844882+Gustavo0Machado@users.noreply.github.com>
2024-09-05 01:03:44 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const { time } = require("console");
+
+let anoAtual = 2024;
+let idade = (16);
+const nomeCompleto = "Gustavo Machado";
+const faculdade = "Aquitetura e Urbanismo";
+let cidadeAtual = "SJP";
+
+
+
+
+console.log(`olá eu sou o ${nomeCompleto} eu tenho ${idade} anos e moro em ${cidadeAtual} e no ano que vem eu espero estar cursando ${faculdade}`);
+
+
+
+anoAtual = 2025;
+idade = 17;
+
+
+console.log(`Para o ano de ${anoAtual} espero já possuir meus bens materiais que anseio e estar cursando e estudando a minha àrea de desejo`);
+
