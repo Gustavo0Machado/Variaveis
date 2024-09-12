f90806f54624377110726d01a98ae565b985ae9f
GustavoMachadolindaoo <127844882+Gustavo0Machado@users.noreply.github.com>
2024-09-12 01:15:40 +0000

    11/09

--- index.js
+++ index.js
@@ -5,16 +5,15 @@ let idade = (16);
 const nomeCompleto = "Gustavo Machado";
 const faculdade = "Aquitetura e Urbanismo";
 let cidadeAtual = "SJP";
-
-
-
+let lazer = "musica";
 
 console.log(`olá eu sou o ${nomeCompleto} eu tenho ${idade} anos e moro em ${cidadeAtual} e no ano que vem eu espero estar cursando ${faculdade}`);
 
-
-
 anoAtual = 2025;
 idade = 17;
+cidadeAtual = "SJP";
+depoisMarço = "Aquitetura e Urbanismo";
+lazer = "musica"
 
 
 console.log(`Para o ano de ${anoAtual} espero já possuir meus bens materiais que anseio e estar cursando e estudando a minha àrea de desejo`);
