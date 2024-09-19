67de5187a711f6089b8fc538aba2243775615585
GustavoMachadolindaoo <127844882+Gustavo0Machado@users.noreply.github.com>
2024-09-19 01:36:48 +0000

    19/09

--- index.js
+++ index.js
@@ -18,3 +18,38 @@ lazer = "musica"
 
 console.log(`Para o ano de ${anoAtual} espero já possuir meus bens materiais que anseio e estar cursando e estudando a minha àrea de desejo`);
 
+//Agência de viagens
+
+const nomeComprador = "Roberto";
+const cpfComprador = 13945678123;
+const idadeComprador = 21;
+const listaDeDestinos = new Array(
+" Ribeirão Preto",
+" Terezina",
+" Osasco",
+" Santa Felicidade",
+" Hong Kong"
+);
+
+console.log(`Olá senhor ${nomeComprador} portador do CPF ${cpfComprador} informamos que sua passagem para   ${listaDeDestinos} foi confirmada, só precisamos da confimação de sua idade, a seguinte procede?  ${idadeComprador} agradecemos sua compreensão!` );
+
+console.log(listaDeDestinos[2]);
+
+console.log("");
+
+console.log(`Olá senhor ${nomeComprador} adcionamos mais uma viagem a sua lista de interesses`);
+
+listaDeDestinos.push(" Tóquio");
+
+console.log(listaDeDestinos);
+
+
+
+
+
+
+
+
+
+
+
